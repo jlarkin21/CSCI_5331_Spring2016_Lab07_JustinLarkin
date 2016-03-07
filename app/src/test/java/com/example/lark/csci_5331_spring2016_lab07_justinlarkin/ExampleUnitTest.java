a161fc2ba5713313ce965da974f18f22b4c7eb27
jlarkin21 <jlarkin9@gmail.com>
2016-03-07 11:42:28 -0300

    test
    test

---
+++ app/src/test/java/com/example/lark/csci_5331_spring2016_lab07_justinlarkin/ExampleUnitTest.java
@@ -0,0 +1,15 @@
+package com.example.lark.csci_5331_spring2016_lab07_justinlarkin;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * To work on unit tests, switch the Test Artifact in the Build Variants view.
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() throws Exception {
+        assertEquals(4, 2 + 2);
+    }
+}
